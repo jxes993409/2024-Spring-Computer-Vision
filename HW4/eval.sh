d1899e6e0ac0926176591e6d46fefb79eb105242
jxes993409 <jxes993409@gmail.com>
2024-05-27 22:43:47 +0800

    Add HW4

---
+++ HW4/eval.sh
@@ -0,0 +1,4 @@
+python3 eval.py --image Tsukuba
+python3 eval.py --image Teddy
+python3 eval.py --image Venus
+python3 eval.py --image Cones
---
+++ HW4/run.sh
@@ -0,0 +1,4 @@
+python3 main.py --image Tsukuba
+python3 main.py --image Teddy
+python3 main.py --image Venus
+python3 main.py --image Cones
